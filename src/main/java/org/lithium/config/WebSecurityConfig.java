711e46cd637cfa96b7612d21cb867264045def0c
Hector Avena <amigoavena@gmail.com>
2014-02-09 13:14:04 +0900

    Cleanup
    Cleaned up code for better understanding.

--- src/main/java/org/lithium/config/WebSecurityConfig.java
+++ src/main/java/org/lithium/config/WebSecurityConfig.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.lithium.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,25 +13,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+			throws Exception {
 		System.out.println("test");
-		auth.inMemoryAuthentication().withUser("user") // #1
-				.password("password").roles("USER").and().withUser("admin") // #2
-				.password("password").roles("ADMIN", "USER");
+		auth.inMemoryAuthentication().withUser("user").password("password")
+				.roles("USER").and().withUser("admin").password("password")
+				.roles("ADMIN", "USER");
 	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**"); // #3
+		web.ignoring().antMatchers("/css/**").antMatchers("/js/**")
+				.antMatchers("/fonts/**");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/signup", "/about").permitAll() // #4
-				.antMatchers("/admin/**").hasRole("ADMIN") // #6
-				.anyRequest().permitAll() // 7
-				.and().formLogin() // #8
-				.permitAll(); // #5
+		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().permitAll().and().formLogin().permitAll();
 	}
 
 }
