015ccf4ab5d53a08ee92180a3244ec9dd5e45967
Hector Avena <amigoavena@gmail.com>
2014-04-07 23:46:23 +0900

    Added Persistence Initial configuration for further development

---
+++ src/main/java/org/lithium/config/PersistenceConfig.java
@@ -0,0 +1,66 @@
+package org.lithium.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+//@ComponentScan({ "org.baeldung.spring.persistence" })
+public class PersistenceConfig {
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory
+				.setPackagesToScan(new String[] { "org.lithium.persistence.domain" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+
+		return sessionFactory;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+				.addScript("sql/initial.sql").build();
+	}
+
+	@Bean
+	public HibernateTransactionManager transactionManager() {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory().getObject());
+		return txManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	Properties hibernateProperties() {
+		return new Properties() {
+			{
+				setProperty("hibernate.hbm2ddl.auto",
+						"create-drop");
+				setProperty("hibernate.dialect",
+						"org.hibernate.dialect.MySQL5Dialect");
+				setProperty("hibernate.globally_quoted_identifiers", "true");
+			}
+		};
+	}
+}
