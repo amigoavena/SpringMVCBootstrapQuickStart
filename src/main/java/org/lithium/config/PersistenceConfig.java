ada02029d47b70e5b31084d38b31894ffd38ad3a
Hector Avena <amigoavena@gmail.com>
2015-04-12 00:26:50 +0900

    Changes for mybatis

--- src/main/java/org/lithium/config/PersistenceConfig.java
+++ src/main/java/org/lithium/config/PersistenceConfig.java
@@ -2,12 +2,16 @@ package org.lithium.config;
 
 import java.util.Properties;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
+import javax.sql.DataSource;
+
+//import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -35,9 +39,12 @@ public class PersistenceConfig {
 	public DataSource dataSource() {
 
 		// http://106.186.23.242/
-		return createPooledDataSource("mysql", "com.mysql.jdbc.Driver",
+		/*return createPooledDataSource("mysql", "com.mysql.jdbc.Driver",
 				"jdbc:mysql://106.186.23.242:3306/gue", "gue_user",
 				"Kzh6wCc5TUpxyKeN");
+		
+		*/
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("sql/initial.sql").build();
 
 		/*
 		 * return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
@@ -57,7 +64,7 @@ public class PersistenceConfig {
 		return new PersistenceExceptionTranslationPostProcessor();
 	}
 
-	private DataSource createPooledDataSource(String dataSourceName,
+	/*private DataSource createPooledDataSource(String dataSourceName,
 			String driverClassName, String jdbcUrl, String jdbcUsername,
 			String jdbcPassword) {
 
@@ -73,10 +80,15 @@ public class PersistenceConfig {
 
 		return ds;
 
-	}
+	}*/
 
-	Properties hibernateProperties() {
+	private Properties hibernateProperties() {
 		return new Properties() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
 			{
 				setProperty("hibernate.hbm2ddl.auto", "create-drop");
 				setProperty("hibernate.dialect",
